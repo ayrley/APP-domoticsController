d34a209fbdb18d9a873005a63cf234a5c9428e62
aleyssens <ayrton.leyssens@soundaz.be>
2026-02-28 12:36:49 +0100

    Added IO class handler

---
+++ src/io.cpp
@@ -0,0 +1,173 @@
+#include <string>
+#include <fstream>
+#include <cerrno>
+#include <iostream>
+
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "io.h"
+
+IO::IO()
+{
+	this->m_exported = false;
+}
+
+IO::~IO()
+{
+}
+
+int IO::exportIo()
+{
+	int fptr;
+	std::string gpioExportLocation = "";
+	std::string exportLocation = "/sys/class/gpio/export";
+	std::string gpioNumber;
+	
+	if (this->m_direction == IO_DIR_AIN) {
+		this->m_exported = true;
+		return 0;
+	}
+	
+	gpioNumber = this->m_location.substr(this->m_location.find("gpio") + 1);
+
+	fptr = open(exportLocation.c_str(), O_WRONLY);
+	if (!fptr)
+		return fptr;
+
+	write(fptr, gpioNumber.c_str(), strlen(gpioNumber.c_str()));
+	close(fptr);
+	
+	gpioExportLocation = this->m_location + "/direction";
+	fptr = open(gpioExportLocation.c_str(), O_WRONLY);
+	
+	if (this->m_direction == IO_DIR_IN)
+		write(fptr, "in" , 2);
+	else if (this->m_direction == IO_DIR_OUT)
+		write(fptr, "out" , 3);
+	
+	close(fptr);
+	
+	return 0;	
+}
+
+void IO::clear()
+{
+	this->set(false);
+}
+
+void IO::set()
+{
+	this->set(true);
+}
+
+void IO::set(bool high)
+{
+	int fptr;
+	std::string gpioValue = this->m_location + "/value";
+	
+	if (!this->m_exported)
+		this->exportIo();
+
+	fptr = open(gpioValue.c_str(), O_WRONLY);
+	if (!fptr) 
+		return;
+
+	write(fptr, high ? "1" : "0", 1);
+	close(fptr);
+
+}
+
+int IO::getAnalogIo()
+{
+	int fptr;
+	char buffer[16] = {0};
+	
+	fptr = open(this->m_location.c_str(), O_RDONLY);
+	if (!fptr) 
+		return -1;
+
+	read(fptr, buffer, 8);
+	close (fptr);
+
+	return std::stoi(buffer);
+}
+
+int IO::getDigitalIo()
+{
+	int fptr;
+	char buffer[16] = {0};
+	std::string gpioValue = this->m_location + "/value";
+	
+	fptr = open(gpioValue.c_str(), O_RDONLY);
+	if (!fptr) 
+		return -1;
+
+	read(fptr, buffer, 5);
+	close (fptr);
+
+	return std::stoi(buffer);
+}
+
+int IO::get()
+{
+	if (!this->m_exported)
+		this->exportIo();
+
+	if (this->m_direction == IO_DIR_AIN)
+		return this->getAnalogIo();
+	else if (this->m_direction == IO_DIR_IN)
+		return this->getDigitalIo();
+
+	return -1;
+}
+
+void IO::fromJson(const json &jsonObject)
+{
+	try {
+		this->m_name = jsonObject["name"];
+	}
+	catch (const std::exception& e) {
+		this->m_name = "";
+		std::cout << "name could not be found in " << jsonObject
+			<< std::endl;
+	}
+	
+	try {
+		this->m_location = jsonObject["location"];
+	}
+	catch (const std::exception& e) {
+		this->m_location = "";
+		std::cout << "location could not be found in " << jsonObject
+			<< std::endl;
+	}
+	
+	try {
+		std::string locationType = jsonObject["type"];
+		if (locationType == "LOCAL")
+			this->m_locationType = IO_LOC_LOCAL;
+		else if (locationType == "IP")
+			this->m_locationType = IO_LOC_IP;
+	}
+	catch (const std::exception& e) {
+		this->m_locationType = IO_LOC_LOCAL;
+		std::cout << "type could not be found in " << jsonObject
+			<< std::endl;
+	}
+	
+	try {
+		std::string direction = jsonObject["direction"];
+		if (direction == "AIN")
+			this->m_direction = IO_DIR_AIN;
+		else if (direction == "IN")
+			this->m_direction = IO_DIR_IN;
+		else if (direction == "OUT")
+			this->m_direction = IO_DIR_OUT;
+	}
+	catch (const std::exception& e) {
+		this->m_direction = IO_DIR_IN;
+		std::cout << "direction could not be found in " << jsonObject
+			<< std::endl;
+	}
+}
---
+++ src/io.h
@@ -0,0 +1,52 @@
+#ifndef __IO_H_
+#define __IO_H_
+
+#include <string>
+#include <mutex>
+
+#include <stdint.h>
+
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+enum ioLocationType {
+	IO_LOC_LOCAL = 0,
+	IO_LOC_IP
+};
+
+enum ioDirection {
+	IO_DIR_IN = 0,
+	IO_DIR_AIN,
+	IO_DIR_OUT
+};
+
+class IO
+{
+private:	
+	std::string m_name;
+	std::string m_location;
+	std::string m_fullLocation;
+	
+	enum ioLocationType m_locationType;
+	enum ioDirection m_direction;
+
+	bool m_exported;
+	
+	int exportIo();
+	int getDigitalIo();
+	int getAnalogIo();
+	
+public:
+	IO();
+	~IO();
+	
+	void fromJson(const json &jsonObject);
+	void clear();
+	void set();
+	void set(bool high);
+	
+	int get();
+};
+
+#endif
