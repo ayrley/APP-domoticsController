bc673e956cef63566783fc92d1bbd44744808e26
ayrley <ayrton.leyssens@soundaz.be>
2026-03-30 12:20:45 +0200

    MetrickBlock: added
    Should be its own component
    
    Signed-off-by: ayrley <ayrton.leyssens@soundaz.be>

---
+++ src/GUI/metricBlock.h
@@ -0,0 +1,17 @@
+#ifndef __METRIC_BLOCK_H
+#define __METRIC_BLOCK_H
+
+#include <string>
+
+namespace nanogui {
+class Label;
+class ProgressBar;
+class Widget;
+}
+
+nanogui::Label *createMetricBlock(nanogui::Widget *parent,
+                                  const std::string &title,
+                                  const std::string &initialValue,
+                                  nanogui::ProgressBar **barOut);
+
+#endif
