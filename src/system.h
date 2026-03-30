e1c1d954fd491a3cca1e92e291174462fa238d7b
ayrley <ayrton.leyssens@soundaz.be>
2026-03-30 11:54:30 +0200

    System: used for retreiving all systemd data
    Stats like RAM, cpu...
    
    Signed-off-by: ayrley <ayrton.leyssens@soundaz.be>

---
+++ src/system.h
@@ -0,0 +1,28 @@
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+#include <string>
+
+class System {
+public:
+
+	struct CpuSample {
+		unsigned long long idle = 0;
+		unsigned long long total = 0;
+	};
+
+	static CpuSample readCpuSample();
+
+	static std::string formatCpuUsage(const CpuSample &previousSample, const CpuSample &currentSample);
+	static double cpuUsageFraction(const CpuSample &previousSample, const CpuSample &currentSample);
+	static std::string readRamUsage();
+	static double readRamUsageFraction();
+	static std::string readDiskUsage();
+	static double readDiskUsageFraction();
+
+private:
+	static std::string formatPercent(double value);
+	static std::string formatGiB(unsigned long long bytes);
+};
+
+#endif
