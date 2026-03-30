30906ce71326783a525b4591d070ee22934e8182
ayrley <ayrton.leyssens@soundaz.be>
2026-03-30 11:55:50 +0200

    Screen: make use of the new GUI dir and the system class
    Signed-off-by: ayrley <ayrton.leyssens@soundaz.be>

---
+++ src/screen.h
@@ -0,0 +1,40 @@
+#ifndef __SCREEN_H
+#define __SCREEN_H
+
+#include <string>
+#include <thread>
+#include <atomic>
+
+namespace nanogui {
+class Screen;
+class Label;
+class ProgressBar;
+}
+
+class Screen {
+private:
+	nanogui::Screen *m_screen;
+
+	nanogui::Label *m_statusLabel;
+	nanogui::Label *m_cpuLabel;
+	nanogui::Label *m_ramLabel;
+	nanogui::ProgressBar *m_cpuBar;
+	nanogui::ProgressBar *m_ramBar;
+
+	std::thread m_statsThread;
+	std::atomic<bool> m_stopStatsThread;
+
+	void startStatsUpdates();
+	void updateSystemStats();
+
+public:
+	Screen(int width = 1024, int height = 768);
+	~Screen();
+
+	void updateStatus(const std::string &status);
+	void render();
+
+	nanogui::Screen *getScreen() const;
+};
+
+#endif
