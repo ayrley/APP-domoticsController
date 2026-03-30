f5621ae476e6b649282b6f744283296650f16f06
ayrley <ayrton.leyssens@soundaz.be>
2026-03-30 09:05:28 +0200

    debug: added debug include This one handles debug messages with a macro Same for errors or logging. This makes it easy to have a default formatting for all debug/log/err messages
    Signed-off-by: ayrley <ayrton.leyssens@soundaz.be>

---
+++ src/debug.h
@@ -0,0 +1,17 @@
+#ifndef __DEBUG_H_
+#define __DEBUG_H_  
+
+#include <iostream>
+
+#define DEBUG
+
+#if defined(DEBUG)
+#define DBG(msg) std::cout << "\tdebug: " <<msg << std::endl;
+#else
+#define DBG(msg)
+#endif
+
+#define ERR(msg) std::cerr << msg << std::endl;
+#define LOG(msg) std::clog << msg << std::endl;
+
+#endif
