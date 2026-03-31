9c3f7733997ae3d9ae1c97527544e5b727eaa03c
ayrley <ayrton.leyssens@soundaz.be>
2026-03-31 13:14:53 +0200

    SettingsPage: go to networksettingsPage
    Signed-off-by: ayrley <ayrton.leyssens@soundaz.be>

--- src/GUI/Pages/settingsPage.cpp
+++ src/GUI/Pages/settingsPage.cpp
@@ -1,6 +1,7 @@
 #include "settingsPage.h"
 
 #include "domeButton.h"
+#include "networkSettingsPage.h"
 
 #include <nanogui/nanogui.h>
 
@@ -12,21 +13,51 @@ SettingsPage::SettingsPage(nanogui::Widget *parent,
     setPageTitle("System Settings");
     contentPanel()->set_layout(new nanogui::GroupLayout(0, 6, 18, 0));
 
+    m_mainSettingsPanel = new nanogui::Widget(contentPanel());
+    m_mainSettingsPanel->set_layout(new nanogui::GroupLayout(0, 6, 18, 0));
+
     auto *desc = new nanogui::Label(
-        contentPanel(),
+        m_mainSettingsPanel,
         "This page is ready for runtime options like update intervals,\n"
         "network endpoints, and device-specific behavior.",
         "sans");
     desc->set_font_size(18);
     desc->set_color(nanogui::Color(130, 215, 225, 255));
 
-    auto *controls = new nanogui::Widget(contentPanel());
+    auto *controls = new nanogui::Widget(m_mainSettingsPanel);
     controls->set_layout(new nanogui::GroupLayout(10, 6, 0, 10));
 
-    auto *saveBtn = new DomeButton(controls, "Save Settings", [onSave]() {
-        if (onSave) {
-            onSave();
-        }
+    auto *networkBtn = new DomeButton(controls, "Network Settings", [this]() {
+        showNetworkSettings();
     });
-    saveBtn->set_fixed_size(nanogui::Vector2i(220, 40));
+    networkBtn->set_fixed_size(nanogui::Vector2i(220, 40));
+
+    m_networkSettingsPage = new NetworkSettingsPage(contentPanel(), [this]() {
+        showMainSettings();
+    });
+    m_networkSettingsPage->set_visible(false);
+
+    showMainSettings();
+}
+
+void SettingsPage::showMainSettings()
+{
+    setPageTitle("System Settings");
+    if (m_mainSettingsPanel) {
+        m_mainSettingsPanel->set_visible(true);
+    }
+    if (m_networkSettingsPage) {
+        m_networkSettingsPage->set_visible(false);
+    }
+}
+
+void SettingsPage::showNetworkSettings()
+{
+    setPageTitle("Network Settings");
+    if (m_mainSettingsPanel) {
+        m_mainSettingsPanel->set_visible(false);
+    }
+    if (m_networkSettingsPage) {
+        m_networkSettingsPage->set_visible(true);
+    }
 }
--- src/GUI/Pages/settingsPage.h
+++ src/GUI/Pages/settingsPage.h
@@ -5,12 +5,26 @@
 
 #include "defaultPage.h"
 
+namespace nanogui
+{
+class Widget;
+} // namespace nanogui
+
+class NetworkSettingsPage;
+
 class SettingsPage : public DefaultPage
 {
 public:
     explicit SettingsPage(nanogui::Widget *parent,
                           std::function<void()> onHome,
                           std::function<void()> onSave);
+
+private:
+    nanogui::Widget *m_mainSettingsPanel{nullptr};
+    NetworkSettingsPage *m_networkSettingsPage{nullptr};
+
+    void showMainSettings();
+    void showNetworkSettings();
 };
 
 #endif
