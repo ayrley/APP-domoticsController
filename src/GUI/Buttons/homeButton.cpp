d7c262f7de99f61a0a7a25e174d5c72fc2147f0d
ayrley <ayrton.leyssens@soundaz.be>
2026-03-31 08:26:30 +0200

    homeButon: refactoring
    Signed-off-by: ayrley <ayrton.leyssens@soundaz.be>

--- src/GUI/Buttons/homeButton.cpp
+++ src/GUI/Buttons/homeButton.cpp
@@ -2,6 +2,6 @@
 
 HomeButton::HomeButton(nanogui::Widget *parent, std::function<void()> callback)
 	: DomeButton(parent, "HOME", std::move(callback)) {
-	set_home_style(true);
+	setHomeStyle(true);
 	set_fixed_size(nanogui::Vector2i(128, 42));
 }
